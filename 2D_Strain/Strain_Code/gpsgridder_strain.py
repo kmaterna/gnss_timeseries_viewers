fix bug with gpsgridder_strain rotation

2D_Strain/Strain_Code/gpsgridder_strain.py
@@ -18,7 +18,7 @@ def compute(myVelfield, MyParams):
 	for i in range(len(myVelfield.n)):
 		outfile.write("%f %f %f %f %f %f 0.0\n" % (myVelfield.elon[i], myVelfield.nlat[i], myVelfield.e[i], myVelfield.n[i], myVelfield.se[i], myVelfield.sn[i]) );
 	outfile.close();
-	subprocess.call("gmt gpsgridder tempgps.txt -R"+MyParams.map_range+" -I"+str(MyParams.grid_inc)+" -S0.5 -Fd0.01 -C0.0025 -Emisfitfile.txt -fg -r -Gnc4_%s.nc",shell=True);  # makes a netcdf grid file
+	subprocess.call("gmt gpsgridder tempgps.txt -R"+MyParams.map_range+" -I"+str(MyParams.grid_inc)+" -S0.5 -Fd0.01 -C0.0005 -Emisfitfile.txt -fg -r -Gnc4_%s.nc",shell=True);  # makes a netcdf grid file
 	# -R = range. -I = interval. -E prints the model and data fits at the input stations (very useful). 
 	# -S = poisson's ratio. -Fd = fudge factor. -C = eigenvalues below this value will be ignored. 
 	# -fg = flat earth approximation. -G = output netcdf files (x and y displacements). 
@@ -68,12 +68,13 @@ def compute(myVelfield, MyParams):
 			vq=vdata[j][i+1];
 			ur=udata[j+1][i];
 			vr=vdata[j+1][i];
+
 			[dudx, dvdx, dudy, dvdy] = strain_tensor_toolbox.compute_displacement_gradients(up, vp, ur, vr, uq, vq, xinc, yinc);
 			
 			# The components that are easily computed
 			# Units: nanostrain per year. 
 			[exx, exy, eyy, rotation] = strain_tensor_toolbox.compute_strain_components_from_dx(dudx, dvdx, dudy, dvdy);
-			rot[j][i]=rotation;
+			rot[j][i]=abs(rotation);
 
 			# Compute a number of values based on tensor properties. 
 			I2nd[j][i] = np.log10(np.abs(strain_tensor_toolbox.second_invariant(exx, exy, eyy)));