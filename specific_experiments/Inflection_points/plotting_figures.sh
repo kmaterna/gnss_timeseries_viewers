82c868e1319eb822dcb75771629afbbdadf3a822
kmaterna <kathrynmaterna@gmail.com>
2019-03-08 12:13:09 -0800

    misc changes for figures

--- specific_experiments/Inflection_points/plotting_figures.sh
+++ specific_experiments/Inflection_points/plotting_figures.sh
@@ -4,5 +4,5 @@
 infile1='Fall2018/20140310.txt'
 infile2='Fall2018/20161208.txt'
 out_ps_name='Figures/combined_2014_2016'
-./timing_figure_combined.gmt $infile1 $infile2 -125 -118 36.5 42.0 $out_ps_name # northern California
+./timing_figure_combined.gmt $infile1 $infile2 -125 -119.4 37.9 42.0 $out_ps_name # northern California
 
--- specific_experiments/afterslip/get_stations_ll.py
+++ specific_experiments/afterslip/get_stations_ll.py
@@ -4,8 +4,10 @@
 
 import stations_within_radius
 import gps_io_functions
+import gps_input_pipeline
+import datetime as dt 
 
-coord_box = [-125, -121, 38.5, 42];
+coord_box = [-125, -120, 42, 46.0];
 infile="../../GPS_POS_DATA/Velocity_Files/NAM08_pbovelfile_feb2018.txt"
 outfile="GPS_ll.txt";
 
@@ -19,6 +21,18 @@ for i in range(len(velfield.name)):
 		if velfield.name[i] in already_added:
 			continue;
 		else:
-			ofile.write("%f %f %s \n" % (velfield.elon[i], velfield.nlat[i], velfield.name[i]) );
+
+			ofile.write("%f %f %s \n" % (velfield.elon[i], velfield.nlat[i], velfield.name[i]) );  # format for afterslip
+			
+			# # IF doing this for GRACE: 
+			# [newData, offset_obj, eq_obj] = gps_input_pipeline.get_station_data(velfield.name[i], 'pbo');			
+			# start = float(dt.datetime.strftime(newData.dtarray[0],"%Y"))-1.0;
+			# end = float(dt.datetime.strftime(newData.dtarray[-1],"%Y"))+1.0;
+			# if start <= 2002: 
+			# 	start=2002; 
+			# if end >= 2018:
+			# 	end=2018;
+			# ofile.write("%s %f %f %.2f %.2f \n" % (newData.name, newData.coords[0], newData.coords[1], start, end ) );  # format for grace
+			
 			already_added.append(velfield.name[i]);
 ofile.close();
