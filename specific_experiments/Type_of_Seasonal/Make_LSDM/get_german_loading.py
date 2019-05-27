765999e4621c57037227d7389fe535193ed51c62
kmaterna <kathrynmaterna@gmail.com>
2019-05-27 13:09:46 -0700

    lsdm small changes

--- specific_experiments/Type_of_Seasonal/Make_LSDM/get_german_loading.py
+++ specific_experiments/Type_of_Seasonal/Make_LSDM/get_german_loading.py
@@ -5,19 +5,22 @@
 # http://rz-vm115.gfz-potsdam.de:8080/repository/entry/show?entryid=362f8705-4b87-48d1-9d86-2cfd1a2b6ac9
 # I use the Center of Figure HYDL products. 
 
-import sys
+import sys, os
 import subprocess
 import datetime as dt 
 import gps_ts_functions
 
 def configure():
-	input_file="norcal_subset.txt";
+	input_file="CA_OR.txt";
 	output_dir="../../../GPS_POS_DATA/PBO_Hydro/LSDM/";
 	return [input_file, output_dir];
 
 def get_stations(input_file, output_dir):
 	ifile=open(input_file,'r');
+	ofile=open("new_stations.txt",'w');
+	ofile.close();
 	for line in ifile:
+		ofile=open("new_stations.txt",'a');
 		temp=line.split();
 		if len(line.split())==0:
 			continue;
@@ -32,9 +35,16 @@ def get_stations(input_file, output_dir):
 		# The actual calling of the script
 		if station_name=="CME6":
 			continue;
+
+		if os.path.isfile(output_dir+station_name+"_LSDM_hydro.txt.txt"): # if we already have the file
+			print("Skipping "+station_name+ " because we already have the file");
+			continue;
+
 		print("Getting LSDM Hydro for station %s " % station_name);
 		print("extractlatlon_bilinintp_remote HYDL CF "+dt.datetime.strftime(start_date,"%Y-%m-%d")+" "+dt.datetime.strftime(end_date,"%Y-%m-%d")+" "+lat+" "+lon+" -o "+output_dir+station_name+"_LSDM_hydro.txt");
 		subprocess.call(["./extractlatlon_bilinintp_remote HYDL CF "+dt.datetime.strftime(start_date,"%Y-%m-%d")+" "+dt.datetime.strftime(end_date,"%Y-%m-%d")+" "+lat+" "+lon+" -o "+output_dir+station_name+"_LSDM_hydro.txt"],shell=True);
+		ofile.write(line+"\n");
+		ofile.close();
 	ifile.close();
 	return;
 
