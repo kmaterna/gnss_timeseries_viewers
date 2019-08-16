d0ba0647a9368a65a430657419754bd5a7e556b2
kmaterna <kathrynmaterna@gmail.com>
2019-08-16 16:18:11 -0700

    re-doing median kernel plot

--- Models/Model_Resolution/mat2gmt.py
+++ Models/Model_Resolution/mat2gmt.py
@@ -11,10 +11,7 @@ el
 nd
 Kern
 sig_dUv
-__version__
 Lons
-__globals__
-d
 dEv
 nd_ll
 sig_dNv
@@ -27,17 +24,16 @@ import numpy as np
 import scipy.io
 
 def configure_and_read():
-	matlab_file="resolution_for_Kathryn.mat";
-	ll_file="2014_inversion_het.mat"
+	matlab_file="forKathryn.mat";
 	output_file="resolution.txt";
 	print("Making text files for %s " % (matlab_file) );
-	return matlab_file, ll_file, output_file;
+	return matlab_file, output_file;
 
 def read_matlab_file(matlab_file):
 	mat = scipy.io.loadmat(matlab_file);
 	return mat;
 
-def write_gmt(mat, ll, output_file):
+def write_gmt(mat, output_file):
 
 	# model_fit = np.dot(mat['Kern'],mat['slip_rate']);
 	# ofile_model = open(output_file_model, 'w');
@@ -45,29 +41,35 @@ def write_gmt(mat, ll, output_file):
 	# 	ofile_model.write("%f %f %f %f\n" % (mat['Lons'][i], mat['Lats'][i], model_fit[i*3], model_fit[i*3+1] ) )
 	# ofile_model.close();
 
-	print(np.shape(ll['slip_rate']));
-	print(np.shape(mat['mean_Kern']));
-	mean_Kern=np.array(mat['mean_Kern']);
-	mean_Kern=mean_Kern.T;
+
+	median_Kern=np.array(mat['median_Kern']).T;
+	median_Kern_area_adjusted=np.array(mat['median_Kern_area_adjusted']).T;	
+	area = np.array(mat['a']).T;
+	mean_area=np.mean(area);
+
+	median_area_adjusted = [(median_Kern[i]/area[i])*mean_area for i in range(len(median_Kern))];
+
+	print(np.max(median_Kern)*1000)
+
 
 	ofile=open(output_file,'w');
-	for i in range(len(mean_Kern)):
-		ofile.write(">-Z%f\n" % (mean_Kern[i]*1000) );  # in mm
-		first_index=ll['el'][i][0]-1;
-		second_index=ll['el'][i][1]-1;
-		third_index=ll['el'][i][2]-1;
+	for i in range(len(median_Kern)):
+		ofile.write(">-Z%f\n" % (median_Kern[i]*1000) );  # in mm
+		# ofile.write(">-Z%f\n" % (median_area_adjusted[i]*1000) );  # in mm
+		first_index=mat['el'][i][0]-1;
+		second_index=mat['el'][i][1]-1;
+		third_index=mat['el'][i][2]-1;
 		limit=42;
-		if ll['nd_ll'][first_index][1]>limit or ll['nd_ll'][second_index][1]>limit or ll['nd_ll'][third_index][1]>limit:
+		if mat['nd_ll'][first_index][1]>limit or mat['nd_ll'][second_index][1]>limit or mat['nd_ll'][third_index][1]>limit:
 			continue;
-		ofile.write("%f %f\n" % (ll['nd_ll'][first_index][0], ll['nd_ll'][first_index][1]) ) # the first point in the triangle 
-		ofile.write("%f %f\n" % (ll['nd_ll'][second_index][0], ll['nd_ll'][second_index][1]) ) # the second point in the triangle
-		ofile.write("%f %f\n" % (ll['nd_ll'][third_index][0], ll['nd_ll'][third_index][1]) ) # the third point in the triangle
+		ofile.write("%f %f\n" % (mat['nd_ll'][first_index][0], mat['nd_ll'][first_index][1]) ) # the first point in the triangle 
+		ofile.write("%f %f\n" % (mat['nd_ll'][second_index][0], mat['nd_ll'][second_index][1]) ) # the second point in the triangle
+		ofile.write("%f %f\n" % (mat['nd_ll'][third_index][0], mat['nd_ll'][third_index][1]) ) # the third point in the triangle
 	ofile.close();
 	return;
 
 if __name__=="__main__":
-	matlab_file, ll_file, output_file=configure_and_read();
+	matlab_file, output_file=configure_and_read();
 	mat = read_matlab_file(matlab_file);
-	ll = read_matlab_file(ll_file);
-	write_gmt(mat, ll, output_file);
+	write_gmt(mat, output_file);
 
--- Models/Model_Resolution/plot_model_resolution.sh
+++ Models/Model_Resolution/plot_model_resolution.sh
@@ -12,10 +12,11 @@ horiz_scale=0.25  # used for velocity change vectors (0.3 sometimes, sometimes s
 projection="M4.5i"  # used for medium experiments.
 
 # Make colorscales
-gmt makecpt -T-10/15/1 -Cocean > datacpt2.cpt
+gmt makecpt -T-0.5/1.2/0.05 -D -Cocean > datacpt2.cpt
+# gmt makecpt -T-10/15/1 -Cocean > datacpt2.cpt
 gmt makecpt -T-29000/8000/500 -Cgray -Z > blue_topo.cpt
 
-gmt pscoast -R$range -J$projection -Slightblue -N1 -N2 -B1.0WeSn:."Mean Kernel": -Dh -K > $output1 # the title goes here
+gmt pscoast -R$range -J$projection -Slightblue -N1 -N2 -B1.0WeSn:."Median Kernel": -Dh -K > $output1 # the title goes here
 gmt grdgradient ../../../Misc/Mapping_Resources/Global_topography_data/ETOPO1_Bed_g_gmt4.grd -A320 -R$range -Getopo1.grad -Nt
 gmt grdhisteq etopo1.grad -Getopo1.hist -N
 gmt grdinfo etopo1.hist 
@@ -27,12 +28,12 @@ gmt pscoast -R$range -J$projection -Wthicker,black -N1 -N2 -Dh -K -O >> $output1
 
 gmt grdcontour ../../Tremor/slab_geometry/cas_slab1.0_clip.grd -R$range -J$projection -Wthicker,purple -C30 -A+f10+ukm+an+ggray80 -GL-124.5/41.8/-120/41.2 -K -O >> $output1
 
-awk '{print $1, $2, $3, $4, $7, $8, $10}' $gpsfile | gmt psvelo -R$range -J$projection -O -K -Se$horiz_scale/0.68/8 -A+e+gblack+pthickest -Wthin,black >> $output1
+awk '{print $1, $2}' $gpsfile | gmt psxy -R$range -J$projection -O -K -Sc0.1 -Gblack -Wthin,black >> $output1
 gmt psvelo -R$range -J$projection -A+e+gblack+pthickest -Se$horiz_scale/0.68/10 -Wblack -K -O <<EOF >> $output1
 -124.1 39.4 2 0 0.0 0.0 0.0 2mm/yr data
 EOF
 
-gmt psscale -R$range -J$projection -DjTR+w12c/0.5c+o-1.5/0.2 -G0/15 -Cdatacpt2.cpt -B5.0:"":/:mm: -P -O >> $output1
+gmt psscale -R$range -J$projection -DjTR+w12c/0.5c+o-1.5/0.2 -G0/1.2 -Cdatacpt2.cpt -B0.5:"":/:mm: -P -O >> $output1
 
 gmt psconvert $output1 -Tg
 open $output1
