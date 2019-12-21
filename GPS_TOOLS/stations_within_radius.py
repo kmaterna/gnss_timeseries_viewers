eacb47defb2c959a67585a85a2e009f8b8fdce34
kmaterna <kathrynmaterna@gmail.com>
2019-12-20 22:16:18 -0800

    select_within_polygon function added

--- GPS_TOOLS/stations_within_radius.py
+++ GPS_TOOLS/stations_within_radius.py
@@ -4,27 +4,48 @@
 
 
 import haversine
+import read_kml
 import gps_io_functions
+import matplotlib.path as mpltPath
+import numpy as np
+
 
 # Reference: Velfield = collections.namedtuple("Velfield",['name','nlat','elon','n','e','u','sn','se','su','first_epoch','last_epoch']);
 
 # DRIVER 1: STATIONS WITHIN RADIUS
-def get_stations_within_radius(center, radius, coord_box=[], network='pbo'):
-	[input_file, center, radius, num_years, max_sigma, coord_box] = configure_circle(center, radius, coord_box, network);
+def get_stations_within_radius(center, radius, coord_box=[-126, -110, 30.0, 49], network='pbo'):  # default coord box is western US
+	[input_file, num_years, max_sigma] = general_config(network);
 	myVelfield = inputs(input_file, num_years, max_sigma, coord_box, network);
 	close_stations, rad_distance = compute_circle(myVelfield, center, radius);
 	return close_stations, rad_distance;
 
 # DRIVER 2: STATIONS WITHIN BOX
 def get_stations_within_box(coord_box, network='pbo'):
-	[input_file, num_years, max_sigma]=configure_box(network);
+	[input_file, num_years, max_sigma]=general_config(network);
 	myVelfield = inputs(input_file, num_years, max_sigma, coord_box, network);
-	close_stations = compute_box(myVelfield, coord_box);
-	return close_stations;
+	within_stations = compute_box(myVelfield, coord_box);
+	return within_stations;
+
+# DRIVER 3: STATIONS WITHIN A USER-DEFINED KML BORDER
+def get_stations_within_polygon(polygon_file, coord_box=[-126, -110, 30.0, 49], network='pbo'):  
+	[input_file, num_years, max_sigma]=general_config(network);
+	[polygon_lon, polygon_lat] = read_kml.read_simple_kml(polygon_file);
+	myVelfield=inputs(input_file, num_years, max_sigma, coord_box, network);
+	within_stations = compute_within_polygon(myVelfield, polygon_lon, polygon_lat);
+	return within_stations;
 
 
-
-
+# ----------- CONFIGURE OPTIONS ---------- # 
+def general_config(network):
+	if network=='pbo' or network=='cwu' or network=='nmt':
+		input_file="../../GPS_POS_DATA/Velocity_Files/NAM08_pbovelfile_feb2018.txt";
+	elif network=='unr':
+		input_file="../../GPS_POS_DATA/Velocity_Files/IGS14_MAGNET_dec2019.txt";
+	else:
+		print("ERROR: Network %s not recognized" % network);
+	num_years=3.0;
+	max_sigma=2.0;
+	return [input_file, num_years, max_sigma];
 
 # ------------ INPUTS ------------------ # 
 def inputs(input_file, num_years, max_sigma, coord_box, network):
@@ -41,19 +62,6 @@ def inputs(input_file, num_years, max_sigma, coord_box, network):
 
 
 # ----------- CIRCLE FUNCTIONS ---------------- # 
-def configure_circle(center, radius, coord_box, network):
-	if network=='pbo' or network=='cwu' or network=='nmt':
-		input_file="../../GPS_POS_DATA/Velocity_Files/NAM08_pbovelfile_feb2018.txt";
-	elif network=='unr':
-		input_file="../../GPS_POS_DATA/Velocity_Files/IGS14_MAGNET_dec2019.txt";
-	else:
-		print("ERROR: Network %s not recognized" % network);
-	num_years=3.0;
-	max_sigma=2.0;
-	if coord_box==[]:
-		coord_box=[-126, -110, 30.0, 49]; # WUS
-	return [input_file, center, radius, num_years, max_sigma, coord_box];
-
 def compute_circle(myVelfield, center, radius):
 	close_stations=[];
 	rad_distance=[];
@@ -66,20 +74,7 @@ def compute_circle(myVelfield, center, radius):
 	return close_stations, rad_distance;
 
 
-
-
-
 # ----------- BOX FUNCTIONS ---------------- # 
-def configure_box(network):
-	if network=='pbo' or network=='cwu' or network=='nmt':
-		input_file="../../GPS_POS_DATA/Velocity_Files/NAM08_pbovelfile_feb2018.txt";
-	elif network=='unr':
-		input_file="../../GPS_POS_DATA/Velocity_Files/IGS14_MAGNET_dec2019.txt";
-	else:
-		print("ERROR: Network %s not recognized" % network);	
-	num_years=3.0;
-	max_sigma=2.0;
-	return [input_file, num_years, max_sigma];
 
 def compute_box(myVelfield, coord_box):
 	close_stations=[];
@@ -91,6 +86,17 @@ def compute_box(myVelfield, coord_box):
 	return close_stations;
 
 
+# ----------- POLYGON FUNCTIONS ---------------- # 
+
+def compute_within_polygon(myVelfield, polygon_lon, polygon_lat):
+	# If within the polygon:
+	polygon = np.row_stack((np.array(polygon_lon), np.array(polygon_lat))).T;
+	points = np.row_stack((np.array(myVelfield.elon), myVelfield.nlat)).T;
+	path = mpltPath.Path(polygon);
+	inside2 = path.contains_points(points);
+	within_stations = [i for (i, v) in zip(myVelfield.name, inside2) if v];
+	print("Returning %d stations that are within the given polygon" % (len(within_stations)) );
+	return within_stations;
 
 
 if __name__=="__main__":
