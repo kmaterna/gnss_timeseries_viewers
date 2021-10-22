9a8ea53623ae08a6f0a807d47024e35d36c33881
kmaterna <kathrynmaterna@gmail.com>
2021-10-21 22:25:42 -0700

    small plotting changes

--- GPS_TOOLS/gps_vel_pygmt_plots.py
+++ GPS_TOOLS/gps_vel_pygmt_plots.py
@@ -16,7 +16,7 @@ def station_vels_to_arrays(station_vels):
     return np.array(elon), np.array(nlat), np.array(e), np.array(n), np.array(u);
 
 
-def simple_pygmt_plot(velfield, outname, symsize=0.1, region=(), horiz_velfield=None):
+def simple_pygmt_plot(velfield, outname, symsize=0.1, region=(), horiz_velfield=None, fault_file=None):
     """Simply plot the displacement vectors from a velocity field in PyGMT, with vertical denoted by colors"""
     lons = [x.elon for x in velfield];
     lats = [x.nlat for x in velfield];
@@ -30,6 +30,8 @@ def simple_pygmt_plot(velfield, outname, symsize=0.1, region=(), horiz_velfield=
     pygmt.makecpt(cmap="polar", series="-5/5/0.25", background="o", output="mycpt.cpt");
     fig.coast(region=region, projection="M7i", frame="0.5", borders='2', shorelines='1.0p,black', water='lightblue',
               map_scale="n0.12/0.12+c" + str(region[2]) + "+w50");
+    if fault_file:
+        fig.plot(data=fault_file, pen='thin,darkgray');
     elon, nlat, e, n, u = station_vels_to_arrays(velfield);
     fig.plot(x=elon, y=nlat, style='c0.04i', color='black', pen='0.4p,white');  # station locations
     fig.plot(x=elon, y=nlat, style='v0.20+e+a40+gblack+h0+p1p,black+z0.04', pen='0.6p,black',
@@ -38,7 +40,7 @@ def simple_pygmt_plot(velfield, outname, symsize=0.1, region=(), horiz_velfield=
         h_elon, h_nlat, h_e, h_n, h_u = station_vels_to_arrays(horiz_velfield);
         fig.plot(x=h_elon, y=h_nlat, style='v0.20+e+a40+gblack+h0+p1p,black+z0.04', pen='0.6p,black',
                  direction=[h_e, h_n]);  # displacement vectors
-    fig.plot(x=elon, y=nlat, style='c'+str(symsize)+'i', G=u, cmap='mycpt.cpt', pen='thin,black');  # vertical
+    fig.plot(x=elon, y=nlat, style='c'+str(symsize)+'i', color=u, cmap='mycpt.cpt', pen='thin,black');  # vertical
     fig.plot(x=region[0] + 0.9, y=region[2] + 0.1, style='v0.20+e+a40+gblack+h0+p1p,black+z0.04', pen='0.6p,black',
              direction=[[20], [0]]);  # scale vector
     fig.text(x=region[0] + 0.5, y=region[2] + 0.1, text="20 mm/yr", font='10p,Helvetica,black')
