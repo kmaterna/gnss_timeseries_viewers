b308ac0b77f09402995f45b9da8d03ee25301538
kmaterna <kathrynmaterna@gmail.com>
2023-09-01 12:08:31 -0700

    removing text from __init__.py

--- gps_tools/file_io/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import io_other
-from . import io_nota
-from . import io_magnet_unr
-from . import io_usgs
-from . import config_io
-from . import gps_input_pipeline
--- test/test_read_files.py
+++ test/test_read_files.py
@@ -1,44 +1,44 @@
 
 import unittest
-import GNSS_TimeSeries_Viewers.gps_tools as gps_tools
+import GNSS_TimeSeries_Viewers.gps_tools.load_gnss as load_gnss
 
 config_file = "/Users/kmaterna/Documents/B_Research/GEOPHYS_DATA/GPS_POS_DATA/config.txt"
 
 class Tests(unittest.TestCase):
 
     def test_unr_database(self):
-        database = gps_tools.load_gnss.create_station_repo(config_file, proc_center='unr', refframe='NA');
+        database = load_gnss.create_station_repo(config_file, proc_center='unr', refframe='NA');
         database.import_full_velfield();
         [_myData, _, _] = database.load_station("P496");
-        database = gps_tools.load_gnss.create_station_repo(config_file, proc_center='unr', refframe='ITRF');
+        database = load_gnss.create_station_repo(config_file, proc_center='unr', refframe='ITRF');
         # database.import_full_velfield();  # TODO
         [_myData, _, _] = database.load_station("P496");
 
     def test_cwu_database(self):
-        database = gps_tools.load_gnss.create_station_repo(config_file, proc_center='cwu', refframe='NA');
+        database = load_gnss.create_station_repo(config_file, proc_center='cwu', refframe='NA');
         database.import_full_velfield();
         [_myData, _, _] = database.load_station("P496");
-        database = gps_tools.load_gnss.create_station_repo(config_file, proc_center='cwu', refframe='ITRF');
+        database = load_gnss.create_station_repo(config_file, proc_center='cwu', refframe='ITRF');
         database.import_full_velfield();
         [myData, _, _] = database.load_station("P496");
         myData.remove_nans();  # testing a computational function
         _slopes = myData.get_slope();
 
     def test_pbo_database(self):
-        database = gps_tools.load_gnss.create_station_repo(config_file, proc_center='pbo', refframe='NA');
+        database = load_gnss.create_station_repo(config_file, proc_center='pbo', refframe='NA');
         database.import_full_velfield();
         [_myData, _, _] = database.load_station("P496");
-        database = gps_tools.load_gnss.create_station_repo(config_file, proc_center='pbo', refframe='ITRF');
+        database = load_gnss.create_station_repo(config_file, proc_center='pbo', refframe='ITRF');
         database.import_full_velfield();
         [_myData, _, _] = database.load_station("P496");
 
     def test_usgs_database(self):
-        database = gps_tools.load_gnss.create_station_repo(config_file, proc_center='usgs', refframe='NA',
-                                                           subnetwork='SFBayArea');
+        database = load_gnss.create_station_repo(config_file, proc_center='usgs', refframe='NA',
+                                                 subnetwork='SFBayArea');
         database.import_full_velfield();
         [_myData, _, _] = database.load_station("P236");
-        database = gps_tools.load_gnss.create_station_repo(config_file, proc_center='usgs', refframe='ITRF',
-                                                           subnetwork='SFBayArea');
+        database = load_gnss.create_station_repo(config_file, proc_center='usgs', refframe='ITRF',
+                                                 subnetwork='SFBayArea');
         [_myData, _, _] = database.load_station("P236");
         database.import_full_velfield();
 
