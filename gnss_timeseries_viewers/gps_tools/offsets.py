653a2df4f5099836508d8037ca5c07ac0c0e4fc8
kmaterna <kathrynmaterna@gmail.com>
2023-12-02 15:49:44 -0800

    adding default Offset values of zero

--- gnss_timeseries_viewers/gps_tools/offsets.py
+++ gnss_timeseries_viewers/gps_tools/offsets.py
@@ -8,41 +8,44 @@ import datetime as dt
 
 class Offset:
     # Defining the offset as a class
-    def __init__(self, e_offset, n_offset, u_offset, evdt):
-        self.e_offset = e_offset;  # in mm.  Each field is a single value.
-        self.n_offset = n_offset;  # in mm
-        self.u_offset = u_offset;  # in mm
-        self.evdt = evdt;   # datetime object
+    def __init__(self, evdt, e_offset=0, n_offset=0, u_offset=0):
+        self.evdt = evdt  # datetime object, required
+        self.e_offset = e_offset  # in mm.  Each field is a single value.
+        self.n_offset = n_offset  # in mm
+        self.u_offset = u_offset  # in mm
 
 
-def remove_offsets(Data0, offsets_obj):
+def remove_offsets(Data0, offsets_list, verbose=False):
     """
     :param Data0: timeseries object
-    :param offsets_obj: list of Offset objects
+    :param offsets_list: list of Offset objects
+    :param verbose: boolean
+    :returns: a timeseries object
     """
-    if len(offsets_obj) == 0:
-        return Data0;
-    newN, newE, newU = [], [], [];
+    if len(offsets_list) == 0:
+        return Data0
+    newN, newE, newU = [], [], []
 
     # Removing offsets
     for i in range(len(Data0.dtarray)):
         # For each day.
-        tempE, tempN, tempU = Data0.dE[i], Data0.dN[i], Data0.dU[i];
-        for item in offsets_obj:
-            # print("removing %f mm from east at %s" % (item.e_offsets, item.evdt));
+        tempE, tempN, tempU = Data0.dE[i], Data0.dN[i], Data0.dU[i]
+        for item in offsets_list:
+            if verbose:
+                print("removing %f mm from east at %s" % (item.e_offsets, item.evdt))
             if Data0.dtarray[i] == item.evdt:  # removing the date of the offset directly (can be messed up)
-                tempE, tempN, tempU = np.nan, np.nan, np.nan;
+                tempE, tempN, tempU = np.nan, np.nan, np.nan
             if Data0.dtarray[i] > item.evdt:
-                tempE = tempE - item.e_offset;
-                tempN = tempN - item.n_offset;
-                tempU = tempU - item.u_offset;
-        newE.append(tempE);
-        newN.append(tempN);
-        newU.append(tempU);
+                tempE = tempE - item.e_offset
+                tempN = tempN - item.n_offset
+                tempU = tempU - item.u_offset
+        newE.append(tempE)
+        newN.append(tempN)
+        newU.append(tempU)
 
     newData = gps_ts_functions.Timeseries(name=Data0.name, coords=Data0.coords, dtarray=Data0.dtarray, dN=newN, dE=newE,
-                                          dU=newU, Sn=Data0.Sn, Se=Data0.Se, Su=Data0.Su, EQtimes=Data0.EQtimes);
-    return newData;
+                                          dU=newU, Sn=Data0.Sn, Se=Data0.Se, Su=Data0.Su, EQtimes=Data0.EQtimes)
+    return newData
 
 
 def fit_single_offset(dtarray, data, interval, offset_num_days):
@@ -55,30 +58,30 @@ def fit_single_offset(dtarray, data, interval, offset_num_days):
     :param interval: [dt, dt] to show where the offset exists
     :param offset_num_days: int, size of the averaging window where pre/post event position will be computed
     """
-    before_indeces, after_indeces = [], [];
+    before_indeces, after_indeces = [], []
 
     # Find the indeces of nearby days
     for i in range(len(dtarray)):
-        deltat_start = dtarray[i] - interval[0];  # the beginning of the interval
-        deltat_end = dtarray[i] - interval[1];  # the end of the interval
+        deltat_start = dtarray[i] - interval[0]  # the beginning of the interval
+        deltat_end = dtarray[i] - interval[1]  # the end of the interval
         if -offset_num_days <= deltat_start.days <= 0:
-            before_indeces.append(i);
+            before_indeces.append(i)
         if 0 <= deltat_end.days <= offset_num_days:
-            after_indeces.append(i);
+            after_indeces.append(i)
 
     # Identify the value of the offset.
     if before_indeces == [] or after_indeces == [] or len(before_indeces) == 1 or len(after_indeces) == 1:
-        offset = 0;
+        offset = 0
         print("Warning: no data before or after offset at %s. Returning offset=0" % (
-            dt.datetime.strftime(interval[0], "%Y-%m-%d")));
+            dt.datetime.strftime(interval[0], "%Y-%m-%d")))
     else:
-        before_mean = np.nanmean([data[x] for x in before_indeces]);
-        after_mean = np.nanmean([data[x] for x in after_indeces]);
-        offset = after_mean - before_mean;
+        before_mean = np.nanmean([data[x] for x in before_indeces])
+        after_mean = np.nanmean([data[x] for x in after_indeces])
+        offset = after_mean - before_mean
         if offset == np.nan:
-            print("Warning: np.nan offset found. Returning 0");
-            offset = 0;
-    return offset;
+            print("Warning: np.nan offset found. Returning 0")
+            offset = 0
+    return offset
 
 
 def solve_for_offsets(ts_object, offset_time_intervals, num_days=10):
@@ -91,25 +94,25 @@ def solve_for_offsets(ts_object, offset_time_intervals, num_days=10):
     :param num_days: int averaging window, default 10 days
     :returns: list of Offset objects
     """
-    print("Solving empirically for offsets at ", offset_time_intervals);
-    Offset_obj = [];
+    print("Solving empirically for offsets at ", offset_time_intervals)
+    Offset_obj = []
     for interval in offset_time_intervals:
         if isinstance(interval, dt.datetime):  # build an interval from a single day.
-            interval = [interval, interval];  # build an interval from a single day.
-        e_offset = fit_single_offset(ts_object.dtarray, ts_object.dE, [interval[0], interval[1]], num_days);
-        n_offset = fit_single_offset(ts_object.dtarray, ts_object.dN, [interval[0], interval[1]], num_days);
-        u_offset = fit_single_offset(ts_object.dtarray, ts_object.dU, [interval[0], interval[1]], num_days);
-        newobj = Offset(e_offset=e_offset, n_offset=n_offset, u_offset=u_offset, evdt=interval[0]);
-        Offset_obj.append(newobj);
-    return Offset_obj;
+            interval = [interval, interval]  # build an interval from a single day.
+        e_offset = fit_single_offset(ts_object.dtarray, ts_object.dE, [interval[0], interval[1]], num_days)
+        n_offset = fit_single_offset(ts_object.dtarray, ts_object.dN, [interval[0], interval[1]], num_days)
+        u_offset = fit_single_offset(ts_object.dtarray, ts_object.dU, [interval[0], interval[1]], num_days)
+        newobj = Offset(evdt=interval[0], e_offset=e_offset, n_offset=n_offset, u_offset=u_offset)
+        Offset_obj.append(newobj)
+    return Offset_obj
 
 
 def filter_offset_list_to_date(offset_list, date_of_interest):
     """Filter a list of possible offsets to only the offset on a particular day of interest."""
     for item in offset_list:
         if item.evdt == date_of_interest:
-            return item;
-    return None;
+            return item
+    return None
 
 
 def table_offset_to_velfield(ts_obj_list, offset_obj_list, target_date):
@@ -124,12 +127,12 @@ def table_offset_to_velfield(ts_obj_list, offset_obj_list, target_date):
     """
     offsetpts = [];
     for i in range(len(offset_obj_list)):
-        offseti = filter_offset_list_to_date(offset_obj_list[i], target_date);
-        tsi = ts_obj_list[i];
+        offseti = filter_offset_list_to_date(offset_obj_list[i], target_date)
+        tsi = ts_obj_list[i]
         if offseti is not None:
-            offsetpts.append(package_offset_as_StationVel(tsi, offseti));
+            offsetpts.append(package_offset_as_StationVel(tsi, offseti))
     print("Found %d look-up-table offsets, %s" % (len(offsetpts), dt.datetime.strftime(target_date, "%Y-%m-%d")));
-    return offsetpts;
+    return offsetpts
 
 
 def manual_offset_to_velfield(ts_obj_list, first_epoch, last_epoch, num_days=10):
@@ -143,12 +146,12 @@ def manual_offset_to_velfield(ts_obj_list, first_epoch, last_epoch, num_days=10)
     :param num_days: averaging window, default to 7 days
     :returns: list of StationVels
     """
-    offsetpts = [];
+    offsetpts = []
     for tsi in ts_obj_list:
-        new_offset = solve_for_offsets(tsi, [[first_epoch, last_epoch]], num_days=num_days)[0];
-        offsetpts.append(package_offset_as_StationVel(tsi, new_offset));
-    print("Solved %d offsets around %s" % (len(offsetpts), dt.datetime.strftime(first_epoch, "%Y-%m-%d")));
-    return offsetpts;
+        new_offset = solve_for_offsets(tsi, [[first_epoch, last_epoch]], num_days=num_days)[0]
+        offsetpts.append(package_offset_as_StationVel(tsi, new_offset))
+    print("Solved %d offsets around %s" % (len(offsetpts), dt.datetime.strftime(first_epoch, "%Y-%m-%d")))
+    return offsetpts
 
 
 def package_offset_as_StationVel(ts_object, offset):
@@ -159,12 +162,12 @@ def package_offset_as_StationVel(ts_object, offset):
     """
     newobj = vel_functions.Station_Vel(name=ts_object.name, nlat=ts_object.coords[1], elon=ts_object.coords[0],
                                        n=offset.n_offset, e=offset.e_offset, u=offset.u_offset, sn=0, se=0, su=0,
-                                       first_epoch='', last_epoch='', meas_type='');
-    return newobj;
+                                       first_epoch='', last_epoch='', meas_type='')
+    return newobj
 
 
 def print_offset_object(Offset_obj):
     for item in Offset_obj:
         print("%s: %.4f mmE, %.4f mmN, %.4f mmU\n" % (dt.datetime.strftime(item.evdt, "%Y-%m-%d"),
-                                                      item.e_offset, item.n_offset, item.u_offset));
-    return;
+                                                      item.e_offset, item.n_offset, item.u_offset))
+    return
