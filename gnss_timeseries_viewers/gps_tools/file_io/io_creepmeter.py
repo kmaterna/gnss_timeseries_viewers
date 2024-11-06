a381ebe75a35b73da6df5497a19124e51cb0d138
kmaterna <kathrynmaterna@gmail.com>
2024-11-06 07:17:25 -0700

    function to read creepmeter data from usgs database

--- gnss_timeseries_viewers/gps_tools/file_io/io_creepmeter.py
+++ gnss_timeseries_viewers/gps_tools/file_io/io_creepmeter.py
@@ -10,7 +10,7 @@ from ..gps_ts_functions import Timeseries
 
 def read_creepmeter(filename) -> Timeseries:
     """
-    Read an excel file for creep-meter timeseries such as those from the Imperial Valley.
+    Read an Excel file for creep-meter timeseries such as those from the Imperial Valley.
 
     :param filename: string, filename
     :return: a TimeSeries object
@@ -86,4 +86,45 @@ def read_creepmeter_ross_road_two_col(filename) -> Timeseries:
         dtarray.append(dt.datetime.strptime(item, "%m/%d/%Y %H:%M:%S"))
     creepmeter_data = Timeseries(name='', coords=[], dtarray=dtarray, dE=num1, dN=zeros, dU=zeros,
                                  Sn=zeros, Se=zeros, Su=zeros)
+    return creepmeter_data
+
+
+def read_creepmeter_nyland_ranch(filename) -> Timeseries:
+    """
+    Read a creepmeter time series from USGS, for example Nyland ranch, downloaded from here:
+    https://earthquake.usgs.gov/monitoring/deformation/data/download.php
+
+    :param filename: string, filename
+    :return: a Timeseries object
+    """
+    print("Reading file %s " % filename)
+    [year, decimal_date, value] = np.loadtxt(filename, unpack=True, usecols=(0, 1, 2))
+    zeros = np.zeros(np.shape(year))
+    dtarray = []
+    for i in range(len(year)):
+        year_str = str(int(year[i]))
+        doy = int(np.floor(decimal_date[i]))
+        fractional_day = decimal_date[i] - doy   # something like 0.347000 (part of a day)
+        number_of_minutes = fractional_day * (60*24)  # number of minutes past midnight
+        hour = int(np.floor(number_of_minutes / 60))
+        minutes = int(np.floor(number_of_minutes)) - hour*60  # minutes past the hour
+        if hour < 10:
+            hour_string = "0" + str(hour)
+        else:
+            hour_string = str(hour)
+        if minutes < 10:
+            minute_string = "0" + str(minutes)
+        else:
+            minute_string = str(minutes)
+        if doy < 10:
+            doy_str = "00"+str(doy)
+        elif doy < 100:
+            doy_str = "0"+str(doy)
+        else:
+            doy_str = str(doy)
+        formatted_datetime = dt.datetime.strptime(str(year_str)+"-"+str(doy_str)+"-"+str(hour_string)
+                                                  + "-" + str(minute_string), "%Y-%j-%H-%M")
+        dtarray.append(formatted_datetime)
+    creepmeter_data = Timeseries(name='', coords=[], dtarray=dtarray, dE=np.array(value),
+                                 dN=zeros, dU=zeros, Se=zeros, Sn=zeros, Su=zeros)
     return creepmeter_data
